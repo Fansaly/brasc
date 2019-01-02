3ba90204f1459e8b719ee354162a0bdc3beb6ae7
fansaly <fansaly@gmail.com>
2019-01-03 03:36:54 +0800

    macos: dock persistent split

---
+++ macOS/system/Dock/_dock_persistent_apps.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# Dock - persistent apps
+
+# https://www.rosettacode.org/wiki/URL_decoding
+urldecode() { local u="${1//+/ }"; printf '%b' "${u//%/\\x}"; }
+
+currentDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+domain=com.apple.dock
+prefDIR=~/Library/Preferences
+plist=$prefDIR/$domain.plist
+
+
+# Custom Dock's persistent apps
+# -----------------------------
+# persistent apps ordered list
+apps=(
+  "Siri"
+  "Launchpad"
+  "Safari"
+  "Google Chrome"
+  "Mail"
+  "Contacts"
+  "Calendar"
+  "Notes"
+  "Reminders"
+  "Maps"
+  "Photos"
+  "Messages"
+  "FaceTime"
+  "iTunes"
+  "iBooks"
+  "App Store"
+  "System Preferences"
+  "Terminal"
+  "Sublime Text"
+)
+
+dictsApple=$(cat "$currentDIR/apps-dicts-apple.plist")
+dicts3rd=$(cat "$currentDIR/apps-dicts-3rd.plist")
+dicts='<dict><key>dicts</key><array/></dict>'
+
+for (( i = 0; i < ${#apps[@]}; i++ )); do
+  app=${apps[$i]}
+
+  # app Dock dict of apple apps
+  dict=$(echo "$dictsApple" | plutil -extract "$app" xml1 -o - -)
+  if [[ $? -ne 0 ]]; then
+    # app Dock dict of 3rd apps
+    dict=$(echo "$dicts3rd" | plutil -extract "$app" xml1 -o - -)
+  fi
+
+  if [[ $? -ne 0 ]]; then
+    echo -e "  \033[0;35m$app\033[0;33m Dock dict\033[0m doesn't exists."
+    continue
+  fi
+
+  # app install directory
+  appDIR=$(urldecode "$( \
+    echo "$dict" | \
+    plutil -extract "tile-data.file-data._CFURLString" xml1 -o - - | \
+    plutil -p - | \
+    sed -e 's/"//g' | \
+    sed -E 's/^file:\/\/(.*)$/\1/' \
+  )" )
+
+  if [[ ! -d "$appDIR" ]]; then
+    echo -e "  \033[0;35m$app\033[0;33m isn't installed.\033[0m"
+    continue
+  fi
+
+  # merge
+  dicts=$(echo "$dicts" | plutil -insert "dicts.$i" -xml "$dict" -o - -)
+done
+
+# extract and save to Dock Preferences
+dicts=$(echo "$dicts" | plutil -extract "dicts" xml1 -o - -)
+plutil -replace "persistent-apps" -xml "$dicts" "$plist"
+
+
+if [[ "$1" != "--norestart" ]]; then
+  killall Dock
+fi
--- macOS/system/Dock/_restore.sh
+++ macOS/system/Dock/_restore.sh
@@ -5,7 +5,8 @@
 _currentDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 source "$_currentDIR/_dock_settings.sh" $1
-source "$_currentDIR/_dock_persistent.sh" $1
+source "$_currentDIR/_dock_persistent_apps.sh" $1
+source "$_currentDIR/_dock_persistent_others.sh" $1
 
 if [[ "$1" != "--norestart" ]]; then
   killall Dock
