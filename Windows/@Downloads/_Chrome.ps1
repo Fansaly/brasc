95e7e100a0eeee415308f11cc9b173eb9be6e416
fansaly <fansaly@gmail.com>
2019-05-16 08:14:24 +0800

    windows: [chore] generate a random uuid

--- Windows/@Downloads/_Chrome.ps1
+++ Windows/@Downloads/_Chrome.ps1
@@ -5,16 +5,21 @@ Param (
 )
 
 
+$IID = "$(New-Guid)".ToUpper()
+
 # https://www.google.com/intl/zh-CN/chrome/?standalone=1
-$URI = 'https://dl.google.com/tag/s/appguid={8A69D345-D564-463C-AFF1-A69D9E530F96}
-&iid={9A785E2B-41EE-D60A-933D-BECD0B6D521D}
+$URI = @"
+https://dl.google.com/tag/s/
+appguid={8A69D345-D564-463C-AFF1-A69D9E530F96}
+&iid={$IID}
 &lang=zh-CN
 &browser=4
 &usagestats=0
 &appname=Google%20Chrome
 &needsadmin=prefers
 &ap=x64-stable-statsdef_1
-&installdataindex=empty/chrome/install/ChromeStandaloneSetup64.exe'
+&installdataindex=empty/chrome/install/ChromeStandaloneSetup64.exe
+"@
 
 $URI = [Uri]::EscapeUriString(($URI -replace '\s',''))
 $OutFile = $URI | Split-Path -Leaf
