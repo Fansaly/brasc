b4dfbdc7bbfb95c803cf2d76193ce6d5e76fa1fb
fansaly <fansaly@gmail.com>
2022-05-03 08:28:15 +0800

    windows: [system] Language fix multiple language input method bug

--- Windows/system/Language/_restore.ps1
+++ Windows/system/Language/_restore.ps1
@@ -1,22 +1,36 @@
-$defaultLang = 'zh-Hans-CN'
-$defaultInputMethod = @{
+$defaultDisplayLang = @{
+  LanguageTag    = 'zh-Hans-CN'
+  InputMethodTip = '0804:{81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E}{FA550B04-5AD7-411F-A5AC-CA038EC515D7}'
+}
+$defaultInputLang = @{
   LanguageTag    = 'en-US'
   InputMethodTip = '0409:00000409'
 }
 
-$langList = Get-WinUserLanguageList | % { $_.LanguageTag }
+$defaultLang = $defaultDisplayLang.LanguageTag
+
+$langList = @(
+  $defaultDisplayLang,
+  $defaultInputLang
+)
+
+$currLangList = Get-WinUserLanguageList | % { $_.LanguageTag }
+
+$newList = @()
+$newList = $currLangList | ? { -not $langList -contains $_ }
+
+$targetList = @()
+$targetList += $langList | % { $_.LanguageTag }
+$targetList += $newList
 
 # sets the language list
-if ($langList[0] -ne $defaultLang) {
-  $newLangList  = @( $defaultLang )
-  $newLangList += $langList | ? { $_ -ne $defaultLang }
-
-  Set-WinUserLanguageList -LanguageList $newLangList -Force
+if ($($currLangList -join ',') -ne $($targetList -join ',')) {
+  Set-WinUserLanguageList -LanguageList $targetList -Force
 }
 
 # sets the default input method override
-if ((Get-WinDefaultInputMethodOverride).InputMethodTip -ne $defaultInputMethod.InputMethodTip) {
-  Set-WinDefaultInputMethodOverride -InputTip $defaultInputMethod.InputMethodTip
+if ((Get-WinDefaultInputMethodOverride).InputMethodTip -ne $defaultInputLang.InputMethodTip) {
+  Set-WinDefaultInputMethodOverride -InputTip $defaultInputLang.InputMethodTip
 }
 
 # use different input method for each app window
