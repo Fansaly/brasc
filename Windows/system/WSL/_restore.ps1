e29a5f122f05201dabcbb258c15a93299dc5e8d1
fansaly <fansaly@gmail.com>
2022-05-05 13:15:26 +0800

    windows: [system] Enable Microsoft-Hyper-V

--- Windows/system/WSL/_restore.ps1
+++ Windows/system/WSL/_restore.ps1
@@ -5,12 +5,21 @@ $PSScriptsPath = (Get-Item -Path $PSScriptRoot).Parent.Parent.FullName + '\.PSSc
 if (!$(Get-PermissionStatus)) { exit }
 
 
-# Enabled Microsoft-Windows-Subsystem-Linux
+# Enable Microsoft-Windows-Subsystem-Linux
 $result = Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux'
-
 if ($result.State -ne 'Enabled') {
   Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' -NoRestart -WarningAction SilentlyContinue | Out-Null
+  $restart = $True
+}
 
+# Enable Microsoft-Hyper-V
+$result = Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V'
+if ($result.State -ne 'Enabled') {
+  Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V' -All -NoRestart -WarningAction SilentlyContinue | Out-Null
+  $restart = $True
+}
+
+if ($restart) {
   Write-Host "`n  - " -ForegroundColor Gray -NoNewLine
   Write-Host "Please restart computer." -ForegroundColor Yellow
 }
