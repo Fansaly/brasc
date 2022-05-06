6b5c9d8c5bef0aa726141f3f78c078a532a9d5af
fansaly <fansaly@gmail.com>
2022-05-06 00:38:21 -0700

    macos: [xcode] fix

--- macOS/_check_xcode.sh
+++ macOS/_check_xcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xcode-select -v >/dev/null 2>&1 || {
-  echo -e "\033[0;91mPlease install \033[0;95mxcode command line tools\033[0m"
+xcode-select -p >/dev/null 2>&1 || {
+  echo -e "\033[0;91mplease install \033[0;95mxcode command line tools\033[0m"
   xcode-select --install
   return 1 &>/dev/null || exit 1
 }
