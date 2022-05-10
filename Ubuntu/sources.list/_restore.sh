c671b83084bf16777f44d440fedef47d3c3be095
fansaly <fansaly@gmail.com>
2022-05-10 21:48:58 +0800

    ubuntu: [sources.list] apply all versions

--- Ubuntu/sources.list/_restore.sh
+++ Ubuntu/sources.list/_restore.sh
@@ -2,13 +2,17 @@
 
 currentDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-fSrc=$currentDIR/sources.list
-fSys=/etc/apt/sources.list
+sources_template=$currentDIR/sources.list.template
+sources_system=/etc/apt/sources.list
+sources_temp=/tmp/sources.list
+codename=$(lsb_release -sc)
 
-md5Src=$(md5sum "${fSrc}" | awk '{print $1}')
-md5Sys=$(md5sum "${fSys}" | awk '{print $1}')
+cat "$sources_template" | sed -E 's/CODENAME/'$codename'/' > "$sources_temp"
 
-if test "$md5Sys" != "$md5Src" && sudo -v; then
-  sudo cp ${fSys} ${fSys}.$(date "+%Y-%m-%d_%H-%M-%S")
-  sudo cp ${fSrc} ${fSys}
+md5_system=$(md5sum "$sources_system" | awk '{print $1}')
+md5_temp=$(md5sum "$sources_temp" | awk '{print $1}')
+
+if test "$md5_system" != "$md5_temp" && sudo -v; then
+  sudo cp "$sources_system" "$sources_system.$(date "+%Y-%m-%d_%H-%M-%S")"
+  sudo mv "$sources_temp" "$sources_system"
 fi
