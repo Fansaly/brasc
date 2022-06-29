d6f06b6cc5dd10b28094df18e49bbf8d2c0f9e91
fansaly <fansaly@gmail.com>
2022-06-29 08:26:38 +0800

    windows: [download] NeteaseMusic update url

--- Windows/@Downloads/_NeteaseMusic.ps1
+++ Windows/@Downloads/_NeteaseMusic.ps1
@@ -4,17 +4,18 @@ Param (
   $UseWebRequest = $False
 )
 
+$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+# $DateTime = [DateTimeOffset]::UtcNow.ToUnixTimeMilliseconds()
+$URI = 'https://music.163.com/api/pc/package/download/latest'
 
-$URI = 'https://music.163.com/api/pc/download/latest'
+$Response = curl.exe -A "$UserAgent" -fIs "$URI"
 
-$Response = curl.exe -fIs "$URI"
 $Location = $Response -match '^Location:\s*http(s)?:\/\/.*$' -replace 'Location:\s*'
 $OutFile = $Location | Split-Path -Leaf
-$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
 
 if ($UseWebRequest) {
-  Invoke-WebRequest -Uri $URI -OutFile $OutFile -UseBasicParsing
+  Invoke-WebRequest -Uri $Location -OutFile $OutFile -UseBasicParsing
 } else {
   Write-Host "`n" -NoNewLine
-  curl.exe -fSL "$URI" -o "$OutFile" -A "$UserAgent"
+  curl.exe -A "$UserAgent" -fSL "$Location" -o "$OutFile"
 }
