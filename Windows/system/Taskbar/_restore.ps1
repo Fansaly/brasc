70f176c70e5edd7d3686a2250a9bfb585e5e2aeb
fansaly <fansaly@gmail.com>
2022-06-29 08:29:25 +0800

    windows: [Taskbar] disabled SearchHistory

--- Windows/system/Taskbar/_restore.ps1
+++ Windows/system/Taskbar/_restore.ps1
@@ -7,9 +7,19 @@ Param (
 
 $RootPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion'
 
-# 任务栏
 $Item = @(
-  # 搜索
+  # 任务栏 → 资讯和兴趣
+  # 0 => 显示图标和文本
+  # 1 => 仅显示图标
+  # 2 => 关闭
+  @{
+    Path = 'Feeds'
+    Name = 'ShellFeedsTaskbarViewMode'
+    Type = 'DWord'
+    Value = 2
+  }
+
+  # 任务栏 → 搜索
   # 0 => 隐藏
   # 1 => 显示搜索图标
   # 2 => 显示搜索框
@@ -20,15 +30,34 @@ $Item = @(
     Value = 0
   }
 
-  # 资讯和兴趣
-  # 0 => 显示图标和文本
-  # 1 => 仅显示图标
-  # 2 => 关闭
+  # 设置 → 隐私和安全 → 搜索权限
+  # ---------------------------
+  # 云内容搜索(Micorosoft 账户)
+  # 0 => 关闭
+  # 1 => 开启
   @{
-    Path = 'Feeds'
-    Name = 'ShellFeedsTaskbarViewMode'
+    Path = 'SearchSettings'
+    Name = 'IsMSACloudSearchEnabled'
     Type = 'DWord'
-    Value = 2
+    Value = 0
+  }
+  # 云内容搜索(工作或学校账户)
+  # 0 => 关闭
+  # 1 => 开启
+  @{
+    Path = 'SearchSettings'
+    Name = 'IsAADCloudSearchEnabled'
+    Type = 'DWord'
+    Value = 0
+  }
+  # 历史记录(搜索历史记录)
+  # 0 => 关闭
+  # 1 => 开启
+  @{
+    Path = 'SearchSettings'
+    Name = 'IsDeviceSearchHistoryEnabled'
+    Type = 'DWord'
+    Value = 0
   }
 )
 
