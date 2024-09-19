65d0b7d0e7247cdffccffce0edb585f603f2b6ba
fansaly <fansaly@gmail.com>
2024-09-19 17:50:01 +0800

    macos: [app] update url of chrome

--- macOS/@Downloads/google-chrome.sh
+++ macOS/@Downloads/google-chrome.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # https://www.google.com/chrome
-url=https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+# https://formulae.brew.sh/cask/google-chrome
+# /api/cask/google-chrome.json (JSON API)
+url=https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg
 output_file=googlechrome.dmg
 
 curl -fSL "$url" -o "$output_file"
