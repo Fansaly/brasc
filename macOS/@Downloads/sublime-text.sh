0f5c196e9f8b904642eaf84ba0de367085a0c032
fansaly <fansaly@gmail.com>
2024-09-19 17:49:07 +0800

    macos: [app] update url of Sublime Text and Package Control

--- macOS/@Downloads/sublime-text.sh
+++ macOS/@Downloads/sublime-text.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-rex='.*href\s*=\s*"(.*(sublime_text_build_(\w*)_mac\.zip))">.*'
+# https://formulae.brew.sh/cask/sublime-text
+ctx=$(curl -fsSL "https://formulae.brew.sh/api/cask/sublime-text.json" | xargs | sed 's/ //g')
 
-ctx=$(curl -fsSL "https://www.sublimetext.com/download")
-ctx=$(echo "$ctx" | grep -m 1 -i "$(echo "$rex" | sed -e 's/[()]//g')")
-
+rex='[\s\S]*(https:.*?_mac\.zip)[\s\S]*'
 url=$(echo "$ctx" | perl -pe "s/${rex}/\1/i")
-version=$(echo "$ctx" | perl -pe "s/${rex}/\3/i")
-output_file=$(echo "$ctx" | perl -pe "s/${rex}/\2/i")
 
-curl -fSL "$url" -o "$output_file"
+if [[ ! "$url" =~ ^http.*mac\.zip$ ]]; then
+  echo -e "\033[0;31minvalid url.\033[0m"
+else
+  output_file=$(echo "$url" | perl -pe "s/.*?([^\/]+\.zip)/\1/i")
+  curl -fSL "$url" -o "$output_file"
+fi
